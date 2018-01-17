3bdb19a32e78bb59247c58a25f1ac2a33fb21a6b
Pedro Ribeiro Pereirinha <pribeirp@usersad.everis.int>
2018-01-17 17:33:31 +0000

    Initial commit

---
+++ src/main/java/com/springplayground/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.springplayground.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping(value = "/user")
+	public String controllerOn(Model model) {
+		
+		model.addAttribute("username", "John Doe");
+		
+		return "user";
+	}
+
+}
